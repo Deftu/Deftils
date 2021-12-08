d5b9e42b82d053ff7f5c700db4df30bd875f77be
Deftu <deftudev@gmail.com>
2021-12-08 12:28:09 +0200

    new: Added hex conversions for colours.

--- src/main/java/xyz/deftu/deftils/Colours.java
+++ src/main/java/xyz/deftu/deftils/Colours.java
@@ -3,6 +3,8 @@ package xyz.deftu.deftils;
 import xyz.deftu.deftils.collections.abstraction.AbstractTriplet;
 import xyz.deftu.deftils.collections.impl.MutableTriplet;
 
+import java.awt.*;
+
 public class Colours {
 
     /**
@@ -11,6 +13,27 @@ public class Colours {
      */
     public static int alphaOf(int colour) {
         return (colour >> 24 & 255);
+    }
+
+    /**
+     * @param colour The RGB colour to convert.
+     * @author AlyssaFox - StackOverflow (https://stackoverflow.com/a/43348258)
+     * @return The hexadecimal equivalent of the colour provided.
+     */
+    public static String toHex(int colour) {
+        StringBuilder hex = new StringBuilder(Integer.toHexString(colour & 0xffffff));
+        while(hex.length() < 6)
+            hex.insert(0, "0");
+        return hex.toString();
+    }
+
+    /**
+     * @param colour The RGB colour to convert.
+     * @author AlyssaFox - StackOverflow (https://stackoverflow.com/a/43348258)
+     * @return The hexadecimal equivalent of the colour provided.
+     */
+    public static String toHex(Color colour) {
+        return toHex(colour.getRGB());
     }
 
     /**
