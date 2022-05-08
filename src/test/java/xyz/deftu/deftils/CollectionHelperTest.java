5566b61b147af10cde589d49fad7055b35733c89
Deftu <deftudev@gmail.com>
2022-05-08 23:01:39 +0200

    Remove multimap test.

--- src/test/java/xyz/deftu/deftils/CollectionHelperTest.java
+++ src/test/java/xyz/deftu/deftils/CollectionHelperTest.java
@@ -4,13 +4,4 @@ import org.junit.jupiter.api.Test;
 
 public class CollectionHelperTest {
 
-    @Test
-    public void arrayListMultiMap() {
-        MultiMap<String, String> multiMap = new ArrayListMultiMap<>();
-        System.out.println(multiMap);
-        multiMap.put("Hello, ", "world");
-        multiMap.put("Hello, ", "developers");
-        System.out.println(multiMap);
-    }
-
 }
